ca6641da35bc1847984bbe4d99059b783a85af7c
wmason1997 <williamcmason1997@gmail.com>
2024-02-17 21:40:20 -0800

    Fixed something in routes/api/index.js (use instead of user) and updated package.json title.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,6 @@ const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
 router.use('thoughts', thoughtRoutes);
-router.user('users', userRoutes);
+router.use('users', userRoutes);
 
 module.exports = router;
